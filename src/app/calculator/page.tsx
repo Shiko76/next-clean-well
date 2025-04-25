d76aff777a8c35041851bde439cdce13649ed6ed
Shiko76 <149859243+Shiko76@users.noreply.github.com>
2025-04-25 11:57:10 +0500

    fix bug

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import CleaningCalculator from '@/components/calculator/Calculator'
 
 export const metadata: Metadata = {
-  title: 'Clean well | Калькулятор уборки, предворительный просчет',
-  description: 'Калькулятор уборки, предворительный просчет',
+  title: 'Clean well | Калькулятор уборки, предварительный просчет',
+  description: 'Калькулятор уборки, предварительный просчет',
 }
 
 export default function Page() {
---
+++ src/app/info/page.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Подробная информация о ценах калькулятора уборки | CleanWell',
+  description: 'Подробная информация о ценах калькулятора уборки | CleanWell'
+}
+
+/*************  ✨ Windsurf Command ⭐  *************/
+/**
+ * Renders a page with detailed information about cleaning calculator prices.
+ */
+
+/*******  99d2bd9e-e2d0-4a9c-b015-68ac373d6319  *******/
+export default function Page() {
+  return <div className='text-black'>информация</div>
+}
